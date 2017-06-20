51aaa0eef6e6acf4f3e6487ba99af9a990c8651d
gabietaylor <gabie.taylor@yahoo.com>
2017-06-20 16:28:49 -0400

    json created before 1st qs logging in cloze and basic

--- BasicCard.js
+++ BasicCard.js
@@ -1,8 +1,17 @@
 var inquirer = require('inquirer');
-console.log(inquirer);
+//console.log(inquirer);
 
 var BasicCard = function(front, back) {
     this.front = front;
     this.back = back;
 };
-console.log(BasicCard);
+//console.log(BasicCard);
+
+var questionOne = new BasicCard(
+    "Who was the first president of the United States?", "George Washington");
+
+// "Who was the first president of the United States?"
+console.log(questionOne.front); 
+
+// "George Washington"
+console.log(questionOne.back); 
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,9 +1,24 @@
 var inquirer = require('inquirer');
-console.log(inquirer);
+//console.log(inquirer);
 
 var ClozeCard = function(fullText, partialText, clozeDeletion) {
     this.fullText      = fullText;
     this.partialText   = partialText;
     this.clozeDeletion = clozeDeletion
 };
-console.log(ClozeCard);
+//console.log(ClozeCard);
+
+var questionOneCloze = new ClozeCard(
+    "George Washington was the first president of the United States.", "George Washington");
+
+// "George Washington"
+console.log(questionOneCloze.cloze); 
+
+// " ... was the first president of the United States.
+console.log(questionOneCloze.partial);
+
+// "George Washington was the first president of the United States.
+console.log(questionOneCloze.fullText);
+
+// Should throw or log an error because "oops" doesn't appear in "This doesn't work"
+var brokenCloze = new ClozeCard("This doesn't work", "oops");
